406e3171a337452b999977eb8f487846902c18aa
JungWKim <lionmini@naver.com>
2020-11-24 23:44:06 +0900

    .

--- RaspberryPi/main.py
+++ RaspberryPi/main.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 import sys
+import pdb
 import termios
 import tty
 import threading
@@ -12,70 +13,55 @@ IN2 = 4
 ENB = 22
 IN3 = 17
 IN4 = 27
-encoderL = 10
-speedL = 60
-speedR = 60
-Kp = 0.1
-steering_signal = 0
+Kp = 0.7
+speedL = 0
+speedR = 0
 target_rpm_gap = 67
 actual_rpm_gap = ""
+steering_signal = 0
+ser = 0
 
 def speed_limit():
     global speedL, speedR
     if speedL > 100:
         speedL = 100
+    elif speedL < 10:
+        speedL = 10
     if speedR > 100:
         speedR = 100
-    if speedL < 10:
-        speedL = 10
-    if speedR < 10:
+    elif speedR < 10:
         speedR = 10
 
 
-def changeSpeed(left = speedL, right = speedR):
-    pwmL.ChangeDutyCycle(left)
-    pwmR.ChangeDutyCycle(right)
-
-
 def speed_calibration():
-    global steering_signal, speedL, speedR, target_rpm_gap, actual_rpm_gap
+    global actual_rpm_gap, steering_signal, speedL, speedR, target_rpm_gap
     while True:
         try:
             actual_rpm_gap = ser.readline().decode('utf-8')
-            actual_rpm_gap = actual_rpm_gap.rstrip('\n')
-            actual_rpm_gap = actual_rpm_gap.rstrip('\r')
+            actual_rpm_gap = actual_rpm_gap.rstrip('\r\n')
             actual_rpm_gap = int(float(actual_rpm_gap))
-            if steering_signal is 1:
-                error = actual_rpm_gap
-                if error > 1:
-                    Pcontrol = Kp * error
-                    speedR += Pcontrol
-                elif error < -1:
-                    Pcontrol = abs(Kp * error)
-                    speedL += Pcontrol
+            if steering_signal is not 0:
+                if steering_signal is 1:
+                    error = actual_rpm_gap
+                    if error > 1:
+                        speedR += Kp * error
+                    elif error < -1:
+                        speedL += abs(Kp * error)
+                elif steering_signal is 2:
+                    error = actual_rpm_gap - target_rpm_gap
+                    if error > 1:
+                        speedR -= Kp * error
+                    elif error < -1:
+                        speedR += abs(Kp * error)
+                elif steering_signal is 3:
+                    error = actual_rpm_gap - target_rpm_gap
+                    if error > 1:
+                        speedL -= Kp * error
+                    elif error < -1:
+                        speedL += abs(Kp * error)
                 speed_limit()
-            elif steering_signal is 2:
-                error = actual_rpm_gap - target_rpm_gap
-                if error > 1:
-                    Pcontrol = Kp * error
-                    speedR -= Pcontrol
-                elif error < -1:
-                    Pcontrol = abs(Kp * error)
-                    speedR += Pcontrol
-                speed_limit()
-            elif steering_signal is 3:
-                error = actual_rpm_gap - target_rpm_gap
-                if error > 1:
-                    Pcontrol = Kp * error
-                    speedL -= Pcontrol
-                elif error < -1:
-                    Pcontrol = abs(Kp * error)
-                    speedL += Pcontrol
-                speed_limit()
-            elif steering_signal is 0:
-                speedL = 0
-                speedR = 0
-            changeSpeed()
+                pwmL.ChangeDutyCycle(speedL)
+                pwmR.ChangeDutyCycle(speedR)
         except serial.SerialException as e:
             pass
 
@@ -94,7 +80,6 @@ def settings():
     GPIO.setup(ENA, GPIO.OUT)
     GPIO.setup(IN1, GPIO.OUT)
     GPIO.setup(IN2, GPIO.OUT)
-    GPIO.setup(encoderL, GPIO.IN)
 
     #Right side
     GPIO.setup(IN3, GPIO.OUT)
@@ -162,8 +147,8 @@ def backward():
 
 
 def main():
-    global speedL, speedR, key, steering_signal, actual_rpm_gap
-
+    global speedL, speedR, steering_signal
+    prev_key = ''
     show_usage()
     settings()
 
@@ -171,73 +156,85 @@ def main():
         while True:
             print("----------------------------------")
             key = getch()
-            if key is 'w':
-                forward()
-                changeSpeed()
-                steering_signal = 1
-            elif key is 'x':
-                backward()
-                changeSpeed()
-                steering_signal = 1
-            elif key is 'a':
-                GPIO.output(IN1, False)
-                GPIO.output(IN2, True)
-                GPIO.output(IN3, True)
-                GPIO.output(IN4, False)
-                changeSpeed()
-                steering_signal = 1
-            elif key is 'd':
-                GPIO.output(IN1, True)
-                GPIO.output(IN2, False)
-                GPIO.output(IN3, False)
-                GPIO.output(IN4, True)
-                changeSpeed()
-                steering_signal = 1
-            elif key is 'q':
-                forward()
-                changeSpeed(20, 60)
-                steering_signal = 2
-            elif key is 'e':
-                forward()
-                changeSpeed(60, 20)
-                steering_signal = 3
-            elif key is 'z':
-                backward()
-                changeSpeed(20, 60)
-                steering_signal = 2
-            elif key is 'c':
-                backward()
-                changeSpeed(60, 20)
-                steering_signal = 3
-            elif key is 's':
-                changeSpeed(0, 0)
-                steering_signal = 0
-            elif key is 'u':
+            if key is not prev_key:
+                if key is 'w':
+                    forward()
+                    speedL = 60
+                    speedR = 60
+                    steering_signal = 1
+                elif key is 'x':
+                    backward()
+                    speedL = 60
+                    speedR = 60
+                    steering_signal = 1
+                elif key is 'a':
+                    GPIO.output(IN1, False)
+                    GPIO.output(IN2, True)
+                    GPIO.output(IN3, True)
+                    GPIO.output(IN4, False)
+                    speedL = speedR = 60
+                    steering_signal = 1
+                elif key is 'd':
+                    GPIO.output(IN1, True)
+                    GPIO.output(IN2, False)
+                    GPIO.output(IN3, False)
+                    GPIO.output(IN4, True)
+                    speedL = 60
+                    speedR = 60
+                    steering_signal = 1
+                elif key is 'q':
+                    forward()
+                    speedL = 20
+                    speedR = 60
+                    steering_signal = 2
+                elif key is 'e':
+                    forward()
+                    speedL = 60
+                    speedR = 20
+                    steering_signal = 3
+                elif key is 'z':
+                    backward()
+                    speedL = 20
+                    speedR = 60
+                    steering_signal = 2
+                elif key is 'c':
+                    backward()
+                    speedL = 60
+                    speedR = 20
+                    steering_signal = 3
+                elif key is 's':
+                    speedL = 0
+                    speedR = 0
+                    steering_signal = 0
+                elif key is chr(27):
+                    #if I don't do this, even if the program stops, the left motor will keep moving
+                    pwmL.ChangeFrequency(0)
+                    pwmR.ChangeFrequency(0)
+                    pwmL.stop()
+                    pwmR.stop()
+                    GPIO.cleanup()
+                    break
+                else:
+                    if key is 'u' or key is 'j':
+                        pass
+                    print("Inappropriate Key!!")
+            if key is 'u':
                 speedL += 10
                 speedR += 10
                 if speedL > 100:
                     speedL = 100
                 if speedR > 100:
                     speedR = 100
-                changeSpeed()
-            elif key is 'j':
+            if key is 'j':
                 speedL -= 10
                 speedR -= 10
                 if speedL < 10:
                     speedL = 10
                 if speedR < 10:
                     speedR = 10
-                changeSpeed()
-            elif key is chr(27):
-                #if I don't do this, even if the program stops, the left motor will keep moving
-                pwmL.ChangeFrequency(0)
-                pwmR.ChangeFrequency(0)
-                pwmL.stop()
-                pwmR.stop()
-                GPIO.cleanup()
-                break
-            else:
-                print("Inappropriate Key!!")
+            pwmL.ChangeDutyCycle(speedL)
+            pwmR.ChangeDutyCycle(speedR)
+            prev_key = key
             print("[Pressed]", key)
             print("[rpm gap]", actual_rpm_gap)
             print("[Left speed]", speedL)
